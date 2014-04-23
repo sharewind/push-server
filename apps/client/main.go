1eac7ed82caab41f2ccd4315a1217e25aa796a91
Caijianfeng <sharewind@gmail.com>
2014-04-23 14:42:24 +0800

    add scripts

--- apps/client/main.go
+++ apps/client/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-	"code.sohuno.com/kzapp/push-server/client"
+	"flag"
 	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"code.sohuno.com/kzapp/push-server/client"
+)
+
+var (
+	flagSet          = flag.NewFlagSet("client", flag.ExitOnError)
+	apiHttpAddress   = flagSet.String("api-http-address", "0.0.0.0:4171", "<addr>:<port> to listen on for HTTP clients")
+	brokerTcpAddress = flagSet.String("broker-tcp-address", "0.0.0.0:8600", "<addr>:<port> to connect broker")
 )
 
 func main() {
+	flagSet.Parse(os.Args[1:])
 	fmt.Println("client start!")
 
-	addr := "127.0.0.1:8600"
 	client_id := int64(451294706224070111)
-	c := client.NewClient(addr, client_id)
-	c.Register("127.0.0.1:4171")
+	c := client.NewClient(*brokerTcpAddress, client_id)
+	c.Register(*apiHttpAddress)
 	c.Connect()
 
 	channel_id := int64(11111)
---
+++ scripts/start_dev.sh
@@ -0,0 +1,4 @@
+mkdir -pv /opt/logs/push/
+nohup /opt/webapps/push/bin/api --http-address="0.0.0.0:4171" --broker-tcp-address="10.10.79.134:8600" &> /opt/logs/push/api.log  &
+nohup /opt/webapps/push/bin/broker --tcp-address="0.0.0.0:8600" --broadcast-address="10.10.79.134:8600"  &> /opt/logs/push/broker.log &
+nohup /opt/webapps/push/bin/worker --http-address="0.0.0.0:8710" --broker-tcp-address="10.10.79.134:8600"  &> /opt/logs/push/worker.log  &
