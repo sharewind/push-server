d792f3395aab2725514b768e8871950072b57191
effyroth <guzhen@effyroths-MacBook-Pro.local>
2014-04-25 09:43:26 +0800

    remove syslog

--- util/log.go
+++ util/log.go
@@ -10,15 +10,11 @@ var log = logging.MustGetLogger("util")
 
 func init() {
 	// Customize the output format
-	logging.SetFormatter(logging.MustStringFormatter("▶ %{level} 0x%{id:x} %{message}"))
+	logging.SetFormatter(logging.MustStringFormatter("%{level} %{message}"))
 	// Setup one stdout and one syslog backend.
 	logBackend := logging.NewLogBackend(os.Stderr, "", stdlog.LstdFlags|stdlog.Lshortfile)
 	logBackend.Color = true
-	syslogBackend, err := logging.NewSyslogBackend("")
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	// Combine them both into one logging backend.
-	logging.SetBackend(logBackend, syslogBackend)
+	logging.SetBackend(logBackend)
 }
